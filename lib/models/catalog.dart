8a96706fab851115aeb855b527621d3e47693883
Rishabh <rishabhgargts@gmail.com>
2022-04-01 23:02:06 +0530

    Item Widget adding

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,10 +1,44 @@
 class Item {
-  final String id;
+  final int id;
   final String name;
   final num price;
   final String desc;
   final String color;
   final String image;
 
-  Item(this.id, this.name, this.price, this.desc, this.color, this.image);
+  Item(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.desc,
+      required this.color,
+      required this.image});
+}
+
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "Dog Meme",
+        price: 999,
+        desc: "Funnnny",
+        color: "#3305a",
+        image:
+            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbreakforbuzz.com%2Fwp-content%2Fuploads%2F2019%2F02%2FImage-dr%25C3%25B4le-du-jour-Mais-non-tes-pas-gros%25E2%2580%25A6.png&f=1&nofb=1"), Item(
+        id: 2,
+        name: "Dog Meme",
+        price: 899,
+        desc: "Funnnny",
+        color: "#3305a",
+        image:
+            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbreakforbuzz.com%2Fwp-content%2Fuploads%2F2019%2F02%2FImage-dr%25C3%25B4le-du-jour-Mais-non-tes-pas-gros%25E2%2580%25A6.png&f=1&nofb=1"),
+             Item(
+        id: 3,
+        name: "Dog Meme",
+        price: 799,
+        desc: "Funnnny",
+        color: "#3305a",
+        image:
+            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fbreakforbuzz.com%2Fwp-content%2Fuploads%2F2019%2F02%2FImage-dr%25C3%25B4le-du-jour-Mais-non-tes-pas-gros%25E2%2580%25A6.png&f=1&nofb=1")
+  ];
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -7,32 +9,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final int days = 0;
-
+    final dummyList = List.generate(
+      CatalogModel.items.length,
+      (i) => CatalogModel.items[i],
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Khamakha'),
         centerTitle: true,
         // backgroundColor: Color.fromARGB(255, 51, 45, 128),
       ),
-      body: Center(
-        // child: Text('Hello World!',
-        //   style: Theme.of(context).textTheme.headline4,
-        // ),
-        child:ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/login');
-          },
-          child: const Text('Login'),
-        ),
-        
-        // child: RaisedButton(
-        //   child: Text('Go to Login Page'),
-        //   onPressed: () {
-        //     Navigator.pushNamed(context, '/login');
-        //   },
-        // ),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (context, index) {
+          return ItemWidget(item: dummyList[index]);
+        },
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
     );
   }
 }
