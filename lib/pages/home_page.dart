ac8df75f67913880d515460abde6276aaf589b21
Rishabh <rishabhgargts@gmail.com>
2022-04-02 16:57:48 +0530

    replacing listVIew with GridView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -51,14 +51,54 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         // backgroundColor: Color.fromARGB(255, 51, 45, 128),
       ),
-      body: (CatalogModel.items.isNotEmpty)? ListView.builder(
-        itemCount: CatalogModel.items.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(item: CatalogModel.items[index]);
-        },
-      ):const Center(
-        child: CircularProgressIndicator(),
-      ),
+      body: (CatalogModel.items.isNotEmpty)
+          ? GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+              ),
+              itemCount: CatalogModel.items.length,
+              itemBuilder: (context, index) {
+                final item = CatalogModel.items[index];
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: GridTile(
+                    header: Container(
+                      child: Text(
+                        item.name,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      
+                      padding: const EdgeInsets.all(12),
+                      decoration: const BoxDecoration(
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                    child: Image.network(item.image,fit: BoxFit.cover,),
+
+                    footer: 
+                    Container(
+                      child: Text(
+                        item.price.toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      
+                      padding: const EdgeInsets.all(12),
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                  ),
+                );
+                // return ItemWidget(item: CatalogModel.items[index]);
+              },
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: const MyDrawer(),
     );
   }
